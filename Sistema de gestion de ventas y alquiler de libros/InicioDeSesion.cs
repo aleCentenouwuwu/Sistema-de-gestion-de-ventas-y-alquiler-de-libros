aef1ad413bfb2743620cfb0bf7f37849f2ac58ed
AlexaniaCenteno <alexaniapauth@gmail.com>
2024-09-11 18:57:36 -0600

    Agregar archivos de proyecto.

---
+++ Sistema de gestion de ventas y alquiler de libros/InicioDeSesion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_gestion_de_ventas_y_alquiler_de_libros
+{
+    public class InicioDeSecion
+    {
+        private bool identidadCorrecta = false;
+
+        //Matrices de usuarios Clientes
+        private string[] identidadCliente = { "2024001", "2024002", "2024003" };
+        private string[] nombresIdentidadClientes = { "anna_101", "marcelo_102", "catalina_103" };
+
+        //matrices de usuarios Docentes
+        private string[] identidadDocente = { "2024004", "2024005", "2024006" };
+        private string[] nombresIdentidadDocentes = { "reina_104", "andres_105", "simon-106" };
+
+        //matrices de usuarios estudiantes
+        private string[] identidadEstudiante = { "2024007", "2024008", "2024009" };
+        private string[] nombresIdentidadEstudiantes = { "nilo_107", "carmen_108", "pankesitosabroso_109" };
+
+        public void _VerificarIdentidad(byte pOpcionInicioDeSecion){
+            identidadCorrecta = false;
+        Console.Clear();
+            Console.WriteLine("Ingrese el nombre de usuario");
+            Console.Write(">>");
+            string nombreUsuarioTM = Console.ReadLine();
+            Console.WriteLine("\nIngrese el ID del usuario");
+            Console.Write(">>");
+            string identidadusuarioTM = Console.ReadLine();
+
+            if (pOpcionInicioDeSecion == 1) {
+                //verifica el ID de clientes
+                if (identidadCliente.Contains(identidadusuarioTM) && nombresIdentidadClientes.Contains(nombreUsuarioTM))
+                {
+                    Console.WriteLine("welcome");
+                    identidadCorrecta = true;
+                }else { Console.Clear(); Console.WriteLine("Error! datos ingresados invalidos"); }
+            }
+            else if (pOpcionInicioDeSecion == 2)
+            {
+                //verifica el ID de docentes
+                if (identidadDocente.Contains(identidadusuarioTM) && nombresIdentidadDocentes.Contains(nombreUsuarioTM))
+                {
+                    Console.WriteLine("welcome");
+                    identidadCorrecta = true;
+                }else { Console.Clear(); Console.WriteLine("Error! datos ingresados invalidos"); }
+            }
+            else if (pOpcionInicioDeSecion == 3)
+            {
+                //verifica el ID de docentes
+                if (identidadEstudiante.Contains(identidadusuarioTM) && nombresIdentidadEstudiantes.Contains(nombreUsuarioTM))
+                {
+                    Console.WriteLine("welcome");
+                    identidadCorrecta = true;
+                }else { Console.Clear(); Console.WriteLine("Error! datos ingresados invalidos"); }
+            }
+        }
+    }
+}
---
+++ Sistema de gestion de ventas y alquiler de libros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_gestion_de_ventas_y_alquiler_de_libros
+{
+    public class Program
+    {
+        InicioDeSecion sesion = new InicioDeSecion();
+        public static void Main(string[] args)
+        {
+            bool salirPrograma = false;
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("------------Inicio de Sesion-----------\n");
+                    Console.WriteLine("[1].  Iniciar sesion como Cliente general");
+                    Console.WriteLine("[2].  Iniciar sesion como Docente");
+                    Console.WriteLine("[3].  Iniciar sesion como Estudiante\n");
+                    Console.WriteLine("[0].  Salir del sistema\n\n");
+                    Console.WriteLine("Nota: Tiene que poseer una tarjeta de identificacion para poder entrar");
+                    byte opc1 = Convert.ToByte(Console.ReadLine());
+                    sesion._VerificarIdentidad(opc1);
+                    /*switch (opc1)
+                    {
+                        case 0:
+                            Console.Clear();
+                            salirPrograma = true;
+                            break;
+                        case 1:
+
+                            break;
+                    }*/
+                }
+                catch (Exception) { }
+            } while (!salirPrograma);
+        }
+    }
+}
